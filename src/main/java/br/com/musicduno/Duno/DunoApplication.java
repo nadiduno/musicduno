7cc8f451add4d9e2b9251caa437adc8408ea0466
nadiduno <nadiduno@gmail.com>
2025-01-07 23:40:34 -0300

    Setup Projet Spring Boot

---
+++ src/main/java/br/com/musicduno/Duno/DunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.musicduno.Duno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/musicduno/Duno/DunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.musicduno.Duno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
